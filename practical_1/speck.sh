2bfde91faba430daa480fd52f4f4749ccec9e360
Matthew <37814869+linkuew@users.noreply.github.com>
2022-12-07 17:01:28 -0500

    first practical upload

---
+++ practical_1/speck.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# speck (SPell chECK) is an simple spell checker written in bash based upon
+# the tutorial found here: http://www.norvig.com/spell-correct.html
+
+# Selection mechanism
+selection () {
+}
+
+# Candidate model
+candidate () {
+}
+
+# Language model
+
+# Error model
+
+# Edits1
+edits1 () {
+        letters = 'abcdefghijklmnopqrstuvwxyz'
+}
+
+# Edits2
+edits2 () {
+        substrings= ()
+        for (( i=0; i<${#word}; i++ )); do
+}
